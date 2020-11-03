f4f97c790df2038bae65187e5bd90c5bee2cd81d
Andrey <andrey.skarlat@gmail.com>
2020-11-03 13:59:54 +0200

    add homework 17, 18

---
+++ 18/Pop Up Box/assets/js/script.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+$(document).ready(function ($) {
+  // Клик по ссылке "Закрыть".
+  $('.popup__close').click(function () {
+    $(this).parents('.popup__fade').fadeOut();
+    return false;
+  }); // Закрытие по клавише Esc.
+
+  $(document).keydown(function (e) {
+    if (e.keyCode === 27) {
+      e.stopPropagation();
+      $('.popup__fade').fadeOut();
+    }
+  }); // Клик по фону, но не по окну.
+
+  $('.popup__fade').click(function (e) {
+    if ($(e.target).closest('.popup').length == 0) {
+      $(this).fadeOut();
+    }
+  });
+});
