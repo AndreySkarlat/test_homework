b37c1d9cba763cdba0a9275332bf87e0846b45f9
Andrey <andrey.skarlat@gmail.com>
2020-12-19 02:12:02 +0200

    add homework js 11

---
+++ JS-11/HomeWork/assets/js/main.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  center: true,
+  margin: 40,
+  nav: true,
+  autoWidth: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  }
+});
